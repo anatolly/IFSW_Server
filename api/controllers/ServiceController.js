20508bb32cae514e213f2afe14a53c20aa2efbd7
atyurin <alt603@mail.ru>
2014-11-22 18:57:03 +0400

    File uploading: initial code

--- api/controllers/ServiceController.js
+++ api/controllers/ServiceController.js
@@ -5,9 +5,33 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+//ALT: Original basic file uploading is get from http://sailsjs.org/#/documentation/concepts/File-Uploads
+
+
+
 module.exports = {
-	//Tolik
-	//Eduard
+		  index: function (req,res){
+
+			    res.writeHead(200, {'content-type': 'text/html'});
+			    res.end(
+			    '<form action="/service/upload" enctype="multipart/form-data" method="post">'+
+			    '<input type="text" name="title"><br>'+
+			    '<input type="file" name="avatar" multiple="multiple"><br>'+
+			    '<input type="submit" value="Upload">'+
+			    '</form>'
+			    )
+			  },
+			  upload: function  (req, res) {
+			    req.file('avatar').upload(function (err, files) {
+			      if (err)
+			        return res.serverError(err);
+
+			      return res.json({
+			        message: files.length + ' file(s) uploaded successfully!',
+			        files: files
+			      });
+			    });
+			  }
 		
 };
 
